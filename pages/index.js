3186b80057013b4601e7a86353c3876ec36aff59
superprogramador31 <h2163782@gmail.com>
2025-04-10 17:29:45 +0000

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function Home() {
+    return <div>
+        <h1> 
+            vovô foi eu que fiz aeeee, 
+            estou muito feliz por ter feito ,eu gostei muito disso
+        </h1>
+    </div>
+}
+
+export default Home;
