af0a34e3f47f6a069c07dd70a17a6e5b7b008df6
superprogramador31 <h2163782@gmail.com>
2025-04-23 20:47:51 +0000

    adiciona scriptis `lint:check`e`lit:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
 function Home() {
-    return <div>
-        <h1> 
-            to feiz demais aeeee
-            
-
-
-        </h1>
+  return (
+    <div>
+      <h1>to feiz demais aeeee</h1>
     </div>
+  );
+}
+
+function teste() {
+  comsole.console.log("indentação errada");
 }
 
 export default Home;
